805e592cadce6569cac1397fa84c016c9d60bd0b
Ahmad Raza <150190431+AhmadR90@users.noreply.github.com>
2026-03-25 00:36:04 +0500

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,79 @@
+// let firstName=10;
+// let lastName=21
+// const total=lastName%firstName
+
+// console.log(total)
+
+// const a = 10;
+// const c= 20;
+
+// (a > c) ? console.log("A is gretaer") : console.log("B is greater");
+
+// a=10
+// console.log("5"==="6")
+// if(a>b){
+//  console.log("A is gretaer than b")
+// }else{
+// console.log("B is greater than a")
+// }
+
+// let a;
+
+// function createUser(name, email) {
+//     // For creating user in the Database
+
+// //   newUser1=User.create(name,email)
+// //      a=newUser
+
+// // for creating dummy user
+//  newUser2={
+//     Name:name,
+//     Email:email
+//  }
+
+//   a=newUser2
+// }
+
+// createUser("user", "user12@gmail.com");
+
+// console.log(a);
+
+// function avg(num1,num2,num3){
+//     const average=(num1+num2+num3)/3
+//     console.log(average)
+// }
+
+// avg(20,30,45)
+
+
+// const check=(num1,num2,num3)=>{
+//    console.log((num1+num2+num3)/3)
+// }
+
+// check(20,30,50)
+// console.log("Before function")
+// const fun1=async()=>{
+//     await console.log("Inside of function")
+// }
+// fun1()
+// console.log("After function")
+
+
+//Arrays
+
+const numbers=[1,"7",6,8,9,0]
+// console.log(numbers.sort())
+// numbers.push(80)
+// numbers[3]=90
+
+//Map
+const result=numbers.map(num=>num+2)
+console.log(result)
+console.log(numbers)
+//Filter
+
+
+//Reduce
+
+
+// console.log(numbers)
